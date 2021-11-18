1d973f9f27e982fdfba96b30b688b29d8c18de51
EnzoAguero <agueroenzo10@gmail.com>
2021-11-18 12:52:15 -0300

    finalizo formularios

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express();
+const port = 3030;
+const path = require('path') //CON ESTO ARMO LAS RUTAS
+
+app.use(express.static('public'))
+app.get('/', (req,res) => res.sendFile(path.join(__dirname,'views','home.html')))         //el req y res son callbacks
+app.get('/contacto',(req,res) => res.sendFile(path.join(__dirname,'views','contacto.html')))
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname,'views', "login.html"))
+})
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname,'views', "register.html"))
+})
+app.listen(port, () => {
+    console.log('Server open as ' + 3030);
+})
+
+
